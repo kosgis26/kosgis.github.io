9472745387717f363b8876961ef3688f86906d54
Kravchuk Kostyntyn <kosgis@gmail.com>
2016-08-31 14:50:26 +0300

    Corected Home Work

--- JavaScript/home_work_13-14/js/script.js
+++ JavaScript/home_work_13-14/js/script.js
@@ -1,58 +1,56 @@
 (function init($) {
+
 	var questions = [
-		"1. Какой формат передачи данных наиболее распостранен?", 
-		"2. Выберите правильный вариант фрагменяа XML файла:", 
-		"3. Выберите правильный вариант фрагмента JSON файла."
+		{
+			'title': '1. Какой формат передачи данных наиболее распостранен?',
+			'answers': [
+				'a. SQL',
+				'b. JSON',
+				'c. XML'
+			],
+			'check': 1
+		},
+		{
+			'title': '2. Выберите правильный вариант фрагменяа XML файла:',
+			'answers': [
+				'a. &lt;greeting/&gtHello, world!;',
+				'b. &lt;greeting&gt;Hello, world!&lt;/&gt;',
+				'c. &lt;greeting&gt;Hello, world!&lt;/greeting&gt;'
+			],
+			'check': 2
+		},
+		{
+			'title': '3. Выберите правильный вариант фрагмента JSON файла.',
+			'answers': [
+				'a. {name: Иван }',
+				'b. {‘name’: ‘Иван’ }',
+				'c. {"name": "Иван" }'
+			],
+			'check': 1
+		}
 	];
-	var answers = [
-		["a. SQL", "b. JSON", "c. XML"], 
-		["a. &lt;greeting/&gtHello, world!;", "b. &lt;greeting&gt;Hello, world!&lt;/&gt;", "c. &lt;greeting&gt;Hello, world!&lt;/greeting&gt;"],
-		["a. {name: Иван }", "b. {‘name’: ‘Иван’ }", 'c. {"name": "Иван" }']
-	];	
 	localStorage.setItem('questions', JSON.stringify(questions));
-	localStorage.setItem('answers', JSON.stringify(answers));
 })(jQuery);
 
 $(document).ready(function() { 
 'use strict'
 	$(function () {
 		var questions = JSON.parse(localStorage.getItem('questions'));
-		var answers = JSON.parse(localStorage.getItem('answers'));
 	
 		var html = $('#opros').html(); 
 		var template = tmpl(html, {
-			questions: questions,
-			answers: answers
+			questions: questions
 		});
-		
+		var box;
+		var show;
 		var modal = "";
 		var body=$('body');
 		
 		body.append(template);
 		
 		$("#result").on('click', function() {
-		// як вказати що вони повинні бути вибраними?
-			
-			var answer0 = $('form[name="question_0"] input[id="lab_1"]')[0];
-			var answer1 = $('form[name="question_1"] input[id="lab_2"]')[0];
-			var answer2 = $('form[name="question_2"] input[id="lab_1"]')[0];
-			
-			var box;
-			var show;
-			
-			if (answer0.checked && answer1.checked && answer2.checked) {
-				modal = "Take my congratulation , yours answers are correct!!!"
-			} else {
-				modal = "Yours answers are false, try again later!!!"
-			};
-			
-			function showModal()  {
-				box = $('<div class="modalBox"></div>');
-				show = $('<div class="modal"><a>"' + modal + '"</a><div class="modalButtom"><input type="submit" id="submit" name="Submit" value="Ok"></div></div>');
-				body.append(box);
-				body.append(show);
-			};
-			// проверка на сделаний ответ на каждий вопрос
+
+			// проверка на сделаный "checked"  на каждий "question"
 			var needAnswer = false;
 			for (var i = 0; i < questions.length; i++) {
 				var inputs = $('form[name="question_' + i + '"] input');
@@ -63,6 +61,7 @@ $(document).ready(function() {
 						hasChecked = true;
 					}
 				}			
+				// выделяем каждий "question" в катором не сделаный "checked" рамкой
 				if (!hasChecked) {
 					needAnswer = true;
 					$('form[name="question_' + i + '"]').css('border', '1px solid red');
@@ -73,7 +72,27 @@ $(document).ready(function() {
 			if (needAnswer) {
 				alert("Сделайте выбор")
 			} else {
-				showModal()
+				// проверка на правильные "answers" на каждый "questions" 
+				var allChecked = true;
+				for ( var i = 0; i < questions.length; i++ ) {
+					var n = questions[i].check;
+					var check = $('#questions'+i+'_lab'+n)[0].checked;
+					if (!check) {
+						allChecked = false;
+						break;
+					}
+				}
+				// уведомление про правильность ответов
+				if (allChecked) {
+					modal = "Take my congratulation , yours answers are correct!!!"
+				} else {
+					modal = "Yours answers are false, try again later!!!"
+				};
+				// создание модального окна с выводом "modal"
+				box = $('<div class="modalBox"></div>');
+				show = $('<div class="modal"><a>"' + modal + '"</a><div class="modalButtom"><input type="submit" id="submit" value="Ok"></div></div>');
+				body.append(box);
+				body.append(show);
 			}
 			// при клике на кнопку "Ok" удаляет модальное окно и чистит форму
 			$('#submit').on('click', function () {
@@ -85,9 +104,5 @@ $(document).ready(function() {
 				};
 			});
 		});
-		function resetModal() {
-			
-		};
-		
 	});
 })
--- JavaScript/home_work_15-16/js/script.js
+++ JavaScript/home_work_15-16/js/script.js
@@ -1,1 +1,74 @@
-﻿
+﻿// https://pixabay.com/api/docs/#default_image_search_response
+$(document).ready(function() {
+
+	var onclick = function() {
+		var searchText = $("#searchEdit").val();
+		var url = "https://pixabay.com/api/";
+		url += "?username=kosgis";
+		url += "&key=3204085-bf48a7e1c1f4d2fd9d2960119";
+		url += "&q=" + searchText;
+		$(".foto").html("Загрузка...");
+		$.ajax({url: url}).then(function(data) {
+			console.log("Data: ", data);
+			var s = "";
+			for(var i = 0; i < data.hits.length; i++) {
+				s += "<img src='" + data.hits[i].userImageURL + "'>";
+			}
+			$(".foto").html(s)
+		})
+	};
+
+	$("#searchButton").on("click", onclick);
+	$("#searchEdit").on("keypress", function(event) {
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			onclick();
+		}
+	});
+
+	var Human = {
+		name: 'Artur', 
+		age: 25, 
+		paul: 'men', 
+		growth: 182, 
+		weight: 75
+	 };
+
+ 	function Worker () {
+		this.placeOfWork = 'office';
+		this.salary = 5000;
+		this.work = function () {
+
+		};
+ 	}
+	function Student () {
+			this.plaseOfStudy = 'university'; 
+			this.scholarship = 1200;
+			this.watchSerials = function () {
+
+			};
+	 	}
+
+ 	var n = 4;
+ 	var newWorker = new Worker;;
+
+ 	newWorker.prototype = Human;
+	console.log("newWorker", newWorker)
+
+	for (var i = 1; i < n; i++) {
+		newWorker '+ i' = newWorker;
+		console.log("newWorker '+ i", newWorker '+ i);
+	}
+
+ 	
+	var newStudent = new Student;
+
+	newStudent.prototype = Human;
+	console.log("newStudent", newStudent)
+
+	// for (var i = 1; i < n; i++) {
+		// newStudent = new Student;
+		// console.log("newStudent", newStudent)
+	// }
+
+});
