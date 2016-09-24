5e82cb57f1a50e24847ea3e0c50c58f30c0eeb6f
Kravchuk Kostyntyn <kosgis@gmail.com>
2016-09-25 00:17:19 +0300

    Final exam

--- Final/src/js/slider.js
+++ Final/src/js/slider.js
@@ -1,48 +1,96 @@
 $(document).ready(function(){
 	function htmSlider(){
 
-		// обертка слайдера
-		let i;
-		
-		var slideWrap = $('.slide-wrap');
-		// ссылки на предудыщий иследующий слайд
-		var nextLink = $('.next-slide');
-		var prevLink = $('.prev-slide');
+
+		var slideWrap1 = $('.slide-wrap1');
+		var slideWrap2 = $('.slide-wrap2');
+		var slideWrap3 = $('.slide-wrap3');
+
+		var nextLink1 = $('.next-slide1');
+		var nextLink2 = $('.next-slide2');
+		var nextLink3 = $('.next-slide3');
+		var prevLink1 = $('.prev-slide1');
+		var prevLink2 = $('.prev-slide2');
+		var prevLink3 = $('.prev-slide3');
 		
 		var is_animate = false;
 		
-		// ширина слайда с отступами
-		var slideWidth = $('.slide-item').outerWidth();
+		var slideWidth1 = $('.slide-item1').outerWidth();
+		var slideWidth2 = $('.slide-item2').outerWidth();
+		var slideWidth3 = $('.slide-item3').outerWidth();
 		
-		// смещение слайдера
-		var newLeftPos = slideWrap.position().left - slideWidth;
+		var newLeftPos1 = slideWrap1.position().left - slideWidth1;
+		var newLeftPos2 = slideWrap2.position().left - slideWidth2;
+		var newLeftPos3 = slideWrap3.position().left - slideWidth3;
 		
-		// Клик по ссылке на следующий слайд
-		nextLink.click(function(){
-			if(!slideWrap.is(':animated')) {
+		nextLink1.click(function(){
+			if(!slideWrap1.is(':animated')) {
 	
-				slideWrap.animate({left: newLeftPos}, 500, function(){
-					slideWrap
-						.find('.slide-item:first')
-						.appendTo(slideWrap)
+				slideWrap1.animate({left: newLeftPos1}, 200, function(){
+					slideWrap1
+						.find('.slide-item1:first')
+						.appendTo(slideWrap1)
 						.parent()
 						.css({'left': 0});
 				});
-
+			}
+		});
+		nextLink2.click(function(){
+			if(!slideWrap2.is(':animated')) {
+	
+				slideWrap2.animate({left: newLeftPos2}, 200, function(){
+					slideWrap2
+						.find('.slide-item2:first')
+						.appendTo(slideWrap2)
+						.parent()
+						.css({'left': 0});
+				});
+			}
+		});
+		nextLink3.click(function(){
+			if(!slideWrap3.is(':animated')) {
+	
+				slideWrap3.animate({left: newLeftPos3}, 200, function(){
+					slideWrap3
+						.find('.slide-item3:first')
+						.appendTo(slideWrap3)
+						.parent()
+						.css({'left': 0});
+				});
 			}
 		});
 
-		// Клик по ссылке на предыдующий слайд
-		prevLink.click(function(){
-			if(!slideWrap.is(':animated')) {
+		prevLink1.click(function(){
+			if(!slideWrap1.is(':animated')) {
 			
-				slideWrap
-					.css({'left': newLeftPos})
-					.find('.slide-item:last')
-					.prependTo(slideWrap)
+				slideWrap1
+					.css({'left': newLeftPos1})
+					.find('.slide-item1:last')
+					.prependTo(slideWrap1)
 					.parent()
-					.animate({left: 0}, 500);
-
+					.animate({left: 0}, 200);
+			}
+		});
+		prevLink2.click(function(){
+			if(!slideWrap2.is(':animated')) {
+			
+				slideWrap2
+					.css({'left': newLeftPos2})
+					.find('.slide-item2:last')
+					.prependTo(slideWrap2)
+					.parent()
+					.animate({left: 0}, 200);
+			}
+		});
+		prevLink3.click(function(){
+			if(!slideWrap3.is(':animated')) {
+			
+				slideWrap3
+					.css({'left': newLeftPos3})
+					.find('.slide-item3:last')
+					.prependTo(slideWrap3)
+					.parent()
+					.animate({left: 0}, 200);
 			}
 		});
 	}
