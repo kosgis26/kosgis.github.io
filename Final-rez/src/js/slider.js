72a3c573e5971bf2180b9efe150defcf3090b3b9
Kravchuk Kostyntyn <kosgis@gmail.com>
2016-09-30 12:22:04 +0300

    med

--- Final-rez/src/js/slider.js
+++ Final-rez/src/js/slider.js
@@ -13,76 +13,51 @@ $(document).ready(function() {
 		var prevLink3 = $('#prev-slide3');
 		
 		var is_animate = false;
-		
-		var slideWidth = $('.slider__item').outerWidth();
-		
-		var newLeftPos1 = slideWrap1.position().left - slideWidth;
-		var newLeftPos2 = slideWrap2.position().left - slideWidth;
-		var newLeftPos3 = slideWrap3.position().left - slideWidth;
+
+		var getWrapShift = function() {			
+			return $(".slider__wrap").position().left - $('.slider__item').outerWidth();	
+		};
+
+		var nextLinkClick = function(sliderWrap) {
+			if (!sliderWrap.is(':animated')) {
+				sliderWrap.animate({left: getWrapShift()}, 500, function() {
+					sliderWrap
+						.find('.slider__item:first')
+						.appendTo(sliderWrap)
+						.parent()
+						.css({'left': 0});
+				});
+			}
+		};
 
 		nextLink1.click(function(){
-			if(!slideWrap1.is(':animated')) {
-				slideWrap1.animate({left: newLeftPos1}, 500, function(){
-					slideWrap1
-						.find('.slider__item:first')
-						.appendTo(slideWrap1)
-						.parent()
-						.css({'left': 0});
-				});
-			}
+			nextLinkClick(slideWrap1);
 		});
-		nextLink2.click(function(){
-			if(!slideWrap2.is(':animated')) {
-				slideWrap2.animate({left: newLeftPos2}, 500, function(){
-					slideWrap2
-						.find('.slider__item:first')
-						.appendTo(slideWrap2)
-						.parent()
-						.css({'left': 0});
-				});
-			}
+		nextLink2.click(function(){			
+			nextLinkClick(slideWrap2);
 		});
 		nextLink3.click(function(){
-			if(!slideWrap3.is(':animated')) {
-				slideWrap3.animate({left: newLeftPos3}, 500, function(){
-					slideWrap3
-						.find('.slider__item:first')
-						.appendTo(slideWrap3)
-						.parent()
-						.css({'left': 0});
-				});
-			}
+			nextLinkClick(slideWrap3);
 		});
 
-		prevLink1.click(function(){
-			if(!slideWrap1.is(':animated')) {			
-				slideWrap1
-					.css({'left': newLeftPos1})
+		var prevLinkClick = function(sliderWrap) {
+			if(!sliderWrap.is(':animated')) {			
+				sliderWrap
+					.css({'left': getWrapShift()})
 					.find('.slider__item:last')
-					.prependTo(slideWrap1)
+					.prependTo(sliderWrap)
 					.parent()
 					.animate({left: 0}, 500);
 			}
+		};
+		prevLink1.click(function(){
+			prevLinkClick(slideWrap1)
 		});
 		prevLink2.click(function(){
-			if(!slideWrap2.is(':animated')) {
-				slideWrap2
-					.css({'left': newLeftPos2})
-					.find('.slider__item:last')
-					.prependTo(slideWrap2)
-					.parent()
-					.animate({left: 0}, 500);
-			}
+			prevLinkClick(slideWrap2)
 		});
 		prevLink3.click(function(){
-			if(!slideWrap3.is(':animated')) {
-				slideWrap3
-					.css({'left': newLeftPos3})
-					.find('.slider__item:last')
-					.prependTo(slideWrap3)
-					.parent()
-					.animate({left: 0}, 500);
-			}
+			prevLinkClick(slideWrap3)
 		});
 	}
 	htmSlider();
