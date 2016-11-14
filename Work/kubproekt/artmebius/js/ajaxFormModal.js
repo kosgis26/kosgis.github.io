b52727063d659ee647843aadb0ac6f312c46cd0b
Kravchuk Kostyntyn <kosgis@gmail.com>
2016-11-14 18:55:21 +0200

    corect_forma

---
+++ Work/kubproekt/artmebius/js/ajaxFormModal.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("#feedbackForm").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Cпасибо!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
